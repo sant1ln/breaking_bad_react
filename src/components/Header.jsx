2393ec5d0c6326f2c98ab6d960ea8c95fbfe6390
Santiago Lopera <santiagoln09@gmail.com>
2020-07-30 00:25:52 -0500

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Header.css';
+
+const Header = () =>(
+    <div className="container">
+        <h1>ReactStore</h1>
+        <div>
+            <input type="text"/>
+        </div>
+    </div>
+)
+export default Header;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Menu = () =>(
+    <nav>
+        
+    </nav>
+)
+export default Menu;
